1dc21d89c1aa78bc924678fd96892bc439c4f431
Vaishnavi <vaishnavic.ece2021@dce.edu.in>
2026-05-04 10:55:54 +0530

    added rest_framework to installed apps

--- smartcart_backend/settings.py
+++ smartcart_backend/settings.py
@@ -22,13 +22,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-  
     'rest_framework', 
-    
+    'django_filters',  
     'corsheaders',
     'api',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', # Static files handling
